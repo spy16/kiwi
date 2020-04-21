001ea6463236b2d7988cb188555159792a2bfcf3
Shivaprasad <shiv.ylp@gmail.com>
2020-04-21 20:06:18 +0530

    Add tests and benchmarks for put, get, scan

--- index/bptree/bptree_test.go
+++ index/bptree/bptree_test.go
@@ -1,8 +1,14 @@
 package bptree
 
-import "testing"
+import (
+	"log"
+	"testing"
+	"time"
 
-func TestBPlusTree_Put(t *testing.T) {
+	"github.com/spy16/kiwi/io"
+)
+
+func TestBPlusTree_Put_Get(t *testing.T) {
 	tree, err := Open(":memory:", false, 0)
 	if err != nil {
 		t.Fatalf("failed to create in-memory tree: %#v", err)
@@ -13,27 +19,98 @@ func TestBPlusTree_Put(t *testing.T) {
 		t.Errorf("expected tree size to be 0, not %d", tree.size)
 	}
 
-	insertKeys := 60
+	insertKeys := 10000
 
-	for i := 0; i < insertKeys; i++ {
-		if err := tree.Put([]byte{byte(i)}, uint64(i)); err != nil {
-			t.Errorf("Put() unexpected error: %#v", err)
+	writeLot(t, tree, insertKeys)
+	if tree.Size() != int64(insertKeys) {
+		t.Errorf("expected tree size to be %d, not %d", insertKeys, tree.Size())
+	}
+
+	readCheck(t, tree, insertKeys)
+	scanLot(t, tree, insertKeys)
+}
+
+func BenchmarkBPlusTree_Put_Get(b *testing.B) {
+	tree, err := Open(io.InMemoryFileName, false, 0)
+	if err != nil {
+		log.Fatalf("failed to init tree: %#v", err)
+	}
+	defer tree.Close()
+
+	b.Run("Put", func(b *testing.B) {
+		var d [4]byte
+		for i := 0; i < b.N; i++ {
+			d[0] = byte(i >> 24)
+			d[1] = byte(i >> 16)
+			d[2] = byte(i >> 8)
+			d[3] = byte(i)
+			_ = tree.Put(d[:], uint64(i))
 		}
-	}
+	})
 
-	if int(tree.size) != insertKeys {
-		t.Errorf("expected tree size to be %d, not %d", insertKeys, tree.size)
-	}
+	b.Run("Get", func(b *testing.B) {
+		var d [4]byte
+		for i := 0; i < b.N; i++ {
+			d[0] = byte(i >> 24)
+			d[1] = byte(i >> 16)
+			d[2] = byte(i >> 8)
+			d[3] = byte(i)
+			_, _ = tree.Get(d[:])
+		}
+	})
+}
 
-	for i := 0; i < insertKeys; i++ {
-		key := []byte{byte(i)}
-		v, err := tree.Get(key)
+func readCheck(t *testing.T, tree *BPlusTree, count int) {
+	start := time.Now()
+
+	b := make([]byte, 4)
+	for i := uint32(0); i < uint32(count); i++ {
+		b[0] = byte(i >> 24)
+		b[1] = byte(i >> 16)
+		b[2] = byte(i >> 8)
+		b[3] = byte(i)
+
+		v, err := tree.Get(b)
 		if err != nil {
-			t.Fatalf("Get('%#x') unexpected error: %#v", key, err)
+			t.Fatalf("Get('%x') unexpected error: %#v", b, err)
 		}
 
 		if v != uint64(i) {
-			t.Errorf("Get() expected value to be %d, not %d", uint64(i), v)
+			t.Fatalf("Get('%x'): %d != %d", b, v, uint64(i))
 		}
 	}
+	t.Logf("read %d keys in %s", tree.Size(), time.Since(start))
+}
+
+func scanLot(t *testing.T, tree *BPlusTree, count int) {
+	start := time.Now()
+	scanned := uint64(0)
+	_ = tree.Scan(nil, func(key []byte, v uint64) error {
+		if v != scanned {
+			t.Fatalf("bad scan for '%x': %d != %d", key, v, scanned)
+		}
+		scanned++
+		return nil
+	})
+
+	if int(scanned) != count {
+		t.Errorf("expected %d keys to be scanned, but scanned only %d", scanned, count)
+	}
+	t.Logf("scanned %d keys in %s", count, time.Since(start))
+}
+
+func writeLot(t *testing.T, tree *BPlusTree, count int) {
+	start := time.Now()
+	b := make([]byte, 4)
+	for i := uint32(0); i < uint32(count); i++ {
+		b[0] = byte(i >> 24)
+		b[1] = byte(i >> 16)
+		b[2] = byte(i >> 8)
+		b[3] = byte(i)
+
+		if err := tree.Put(b, uint64(i)); err != nil {
+			panic(err)
+		}
+	}
+	t.Logf("inserted %d (expected=%d) keys in %s", tree.Size(), count, time.Since(start))
 }
